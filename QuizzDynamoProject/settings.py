e0af725511675bb80a44ca989d5c3962030092a4
Vinay Sriram Iyer <x23203595@student.ncirl.ie>
2024-04-04 09:53:25 +0000

    static

--- QuizzDynamoProject/settings.py
+++ QuizzDynamoProject/settings.py
@@ -126,13 +126,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATICFILES_DIRS += [
-    os.path.join(BASE_DIR, 'QuizzDynamoApp', 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
